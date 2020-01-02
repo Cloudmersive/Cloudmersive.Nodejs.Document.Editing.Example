fbdf02f510bae6dc5f8b79a61e9402c5efce6353
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2020-01-02 10:43:20 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+var CloudmersiveConvertApiClient = require('cloudmersive-convert-api-client');
+var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
+
+// Configure API key authorization: Apikey
+var Apikey = defaultClient.authentications['Apikey'];
+Apikey.apiKey = 'YOUR API KEY';
+
+
+
+var apiInstance = new CloudmersiveConvertApiClient.EditDocumentApi();
+
+var inputFile = Buffer.from(fs.readFileSync("C:\\temp2\\input.xlsx").buffer); // File | Input file to perform the operation on.
+
+
+var callback = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+};
+apiInstance.editDocumentBeginEditing(inputFile, callback);
