fb8c2f4c71818e508491505a4fbbdaab38804694
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2020-01-02 11:41:20 -0800

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
 
 // Configure API key authorization: Apikey
 var Apikey = defaultClient.authentications['Apikey'];
-Apikey.apiKey = 'YOUR API KEY';
-
+Apikey.apiKey = 'YOUR-API-KEY';
 
 
 var apiInstance = new CloudmersiveConvertApiClient.EditDocumentApi();
@@ -19,6 +18,20 @@ var callback = function(error, data, response) {
     console.error(error);
   } else {
     console.log('API called successfully. Returned data: ' + data);
+
+    var input = new CloudmersiveConvertApiClient.GetXlsxRowsAndCellsRequest(); // GetXlsxRowsAndCellsRequest | Document input request
+
+    input.InputFileUrl = data;
+
+    var callback2 = function(error, data, response2) {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log('API called successfully. Returned data: ' + data);
+        }
+      };
+      apiInstance.editDocumentXlsxGetRowsAndCells(input, callback2);
+
   }
 };
 apiInstance.editDocumentBeginEditing(inputFile, callback);
