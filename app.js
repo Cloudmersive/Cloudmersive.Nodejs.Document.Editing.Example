bfd640bdecfde9d39cb193a41e1d157681f63136
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2020-01-02 23:18:04 -0800

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var apiInstance = new CloudmersiveConvertApiClient.EditDocumentApi();
 
 var inputFile = Buffer.from(fs.readFileSync("C:\\temp2\\input.xlsx").buffer); // File | Input file to perform the operation on.
 
+// Read an XLSX
 
 var callback = function(error, data, response) {
   if (error) {
@@ -35,3 +36,24 @@ var callback = function(error, data, response) {
   }
 };
 apiInstance.editDocumentBeginEditing(inputFile, callback);
+
+// Create an XLSX
+
+var apiInstance = new CloudmersiveConvertApiClient.EditDocumentApi();
+
+var input = new CloudmersiveConvertApiClient.CreateSpreadsheetFromDataRequest(); // CreateSpreadsheetFromDataRequest | Document input request
+
+input.Rows = new CloudmersiveConvertApiClient.XlsxSpreadsheetRow[1];
+input.Rows[0] = new CloudmersiveConvertApiClient.XlsxSpreadsheetRow();
+input.Rows[0].Cells = new CloudmersiveConvertApiClient.XlsxSpreadsheetCell[1];
+input.Rows[0].Cells[0].CellIdentifier = "A1";
+input.Rows[0].Cells[0].TextValue = "Hello, world";
+
+var callback3 = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API editDocumentXlsxCreateSpreadsheetFromData called successfully. Returned data: ' + data);
+  }
+};
+apiInstance.editDocumentXlsxCreateSpreadsheetFromData(input, callback3);
