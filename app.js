ce6d046fa910b36d3abcdab9b854d7b4c00140da
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2020-01-02 23:29:56 -0800

    Added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
 
 // Configure API key authorization: Apikey
 var Apikey = defaultClient.authentications['Apikey'];
-Apikey.apiKey = 'YOUR-API-KEY';
+Apikey.apiKey = 'YOUR-API-KEY-HERE';
 
 
 var apiInstance = new CloudmersiveConvertApiClient.EditDocumentApi();
@@ -43,9 +43,10 @@ var apiInstance = new CloudmersiveConvertApiClient.EditDocumentApi();
 
 var input = new CloudmersiveConvertApiClient.CreateSpreadsheetFromDataRequest(); // CreateSpreadsheetFromDataRequest | Document input request
 
-input.Rows = new CloudmersiveConvertApiClient.XlsxSpreadsheetRow[1];
+input.Rows = [];
 input.Rows[0] = new CloudmersiveConvertApiClient.XlsxSpreadsheetRow();
-input.Rows[0].Cells = new CloudmersiveConvertApiClient.XlsxSpreadsheetCell[1];
+input.Rows[0].Cells = [];
+input.Rows[0].Cells[0] = new CloudmersiveConvertApiClient.XlsxSpreadsheetCell();
 input.Rows[0].Cells[0].CellIdentifier = "A1";
 input.Rows[0].Cells[0].TextValue = "Hello, world";
 
@@ -54,6 +55,30 @@ var callback3 = function(error, data, response) {
     console.error(error);
   } else {
     console.log('API editDocumentXlsxCreateSpreadsheetFromData called successfully. Returned data: ' + data);
+
+
+    var reqConfig = new CloudmersiveConvertApiClient.FinishEditingRequest(); // FinishEditingRequest | Cloudmersive Document URL to complete editing on
+    reqConfig.InputFileUrl = data.EditedDocumentURL;
+
+    var callback5 = function(error, data, response) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log('API FinishEditingRequest called successfully. Returned data: ' + data);
+
+        fs.writeFile('C:\\temp\\demo.xlsx', data, (err) => { 
+      
+            // In case of a error throw err. 
+            if (err) throw err; 
+        });
+    }
+    };
+    apiInstance.editDocumentFinishEditing(reqConfig, callback5);
+
+
+
+
+    
   }
 };
 apiInstance.editDocumentXlsxCreateSpreadsheetFromData(input, callback3);
